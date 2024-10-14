b158f4fc2616d47e7c89ea511f75b69d253ac50a
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-10-14 13:17:36 +0500

    Base model is Prepared

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from cnnClassifier.logger import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.exception import CustomException
-import sys
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion Stage"
 
+
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nX============X")
-
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise CustomException(e, sys)
+        logger.exception(e)
+        raise e
+
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,8 +2,8 @@ from cnnClassifier.constants import *
 from cnnClassifier.utils.utils import read_yaml, create_directories
 from cnnClassifier.exception import CustomException
 import sys
-from cnnClassifier.logger import logging
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.logger import logger
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 
 class ConfigurationManager:
     
@@ -32,7 +32,7 @@ class ConfigurationManager:
             self.params = read_yaml(params_filepath)
         
         except Exception as e:
-            logging.error(f"Failed to read YAML files: {e}")
+            logger.error(f"Failed to read YAML files: {e}")
             raise CustomException(e, sys)
         
         create_directories([self.config.artifacts_root])
@@ -68,3 +68,24 @@ class ConfigurationManager:
 
         except Exception as e:
             raise CustomException(e, sys)
+        
+
+        
+    def get_prepare_base_model(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_classes=self.params.CLASSES,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_weights=self.params.WEIGHTS
+        )
+        
+        return prepare_base_model_config
+        
